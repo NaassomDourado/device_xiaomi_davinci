205e76c23a12abdb643370f92f9fd8beb2ef9ed3
Patrick Tjin <pattjin@google.com>
2021-08-03 19:48:30 +0200

    davinci: add nosdcard characteristic to device
     * Just as per stock.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ieb8d628237f64155077ef5d351892e1646aaceb8

--- device.mk
+++ device.mk
@@ -40,6 +40,8 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
