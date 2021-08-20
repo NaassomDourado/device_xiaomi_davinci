738a564430f922e98329c8dcb25918df7a032a3f
NaassomDourado <naassomdourado@gmail.com>
2021-08-20 12:03:28 -0300

    davinci: Don't build ANX

--- device.mk
+++ device.mk
@@ -23,8 +23,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 
-# ANXCamera
-$(call inherit-product, vendor/aeonax/ANXCamera/anx-vendor.mk)
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
